483918f1531b1e63c202194335b98fab00023ea3
Aaron Jensen <aaronjensen@gmail.com>
2016-08-08 06:32:43 -0700

    Add react-styleguidist w/ support for webpack 2

---
+++ styleguide.config.js
@@ -0,0 +1,20 @@
+const path = require('path')
+
+module.exports = {
+  title: 'My Style Guide',
+  components: './web/static/js/components/**/*.js',
+  updateWebpackConfig: function(webpackConfig, env) {
+    // Your source files folder or array of folders, should not include node_modules
+    let dir = path.join(__dirname, 'web/static/js/components');
+    webpackConfig.module.loaders.push(
+      // Babel loader will use your project’s .babelrc
+      {
+        test: /\.jsx?$/,
+        include: dir,
+        loader: 'babel'
+      }
+    )
+
+    return webpackConfig
+  },
+}
